0f1c50dea2bf8a516dce0400a1831cd1ab983b79
i.nikushev1@gmail.com <i.nikushev1@gmail.com@f10a737b-d75b-ed3a-ee7e-1704f8fe563d>
2015-03-10 23:14:19 +0000

    PayPalDocument Tests

---
+++ SOURCE/ClassesTest/PayPalDocumentTest.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Classes
+{
+    [TestClass]
+    public class PayPalDocumentTest
+    {
+        [TestMethod]
+        public void ValidDocument()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void InvalidNumberOfLines()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void AccountNumberNotExist()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void AccountNumberTooBig()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void AccountNumberTooSmall()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void AccountNumberTooSmall()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void DateTimeFormat()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void InvalidStartDate()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void InvalidStartDateTime()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void InvalidEndDate()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void InvalidEndDateTime()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void InvalidNumberOfDeposits()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void InvalidDepositNoSpace()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void InvalidDepositWithLetters()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void InvalidTooManyDeposits()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void InvalidDepositTooBig()
+        {
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exceptions.InvalidPayPalLogFileException))]
+        public void InvalidDepositTooSmall()
+        {
+        }
+    }
+}
