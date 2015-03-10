f997c548a0850de7f18a2f59815eb2d3dbef64d6
i.nikushev1@gmail.com <i.nikushev1@gmail.com@f10a737b-d75b-ed3a-ee7e-1704f8fe563d>
2015-03-10 19:09:49 +0000

    Applications v1 Separated into 3 projects Classes holds the core logic for the classes ClassesTest is an UNIT TEST project to test the logic in the classes Design is where the designs of the applications will be and use the CLASSESS project's classes

---
+++ SOURCE/Classes/Exceptions/InvalidShopException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Exceptions
+{
+    [Serializable]
+    public class InvalidShopException : Exception
+    {
+        public InvalidShopException() { }
+        public InvalidShopException(string message) : base(message) { }
+        public InvalidShopException(string message, Exception inner) : base(message, inner) { }
+        protected InvalidShopException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
---
+++ SOURCE/Classes/Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public abstract class Item
+    {
+        //public Image Icon{get; private set;}
+        private decimal price;
+        public decimal Price
+        {
+            get { return this.price; }
+            set
+            {
+                if (value <= 0)
+                    throw new InvalidOperationException("Price cannot be lower or equal to 0. Price was " + value);
+                else this.price = value;
+            }
+        }
+        public int ID { get; private set; }
+        public string Name { get; private set; }
+
+        public Item(int id, string name, decimal price)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Price = price;
+        }
+    }
+}
