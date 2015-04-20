97b7662059f2d45f0e45f039f1c6ba0739457e6b
Iliq <iliq.nikushev@hotmail.com>
2015-04-20 11:00:35 +0200

    Merge

---
+++ SOURCE/Design/Idea/SubMenuBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Design.Idea
+{
+    public partial class SubMenuBase : Form
+    {
+        public SubMenuBase() { InitializeComponent(); }
+        public SubMenuBase(Form parent)
+        {
+            parent.Hide();
+            this.FormClosed += (x, y) => parent.Show();
+            InitializeComponent();
+        }
+    }
+}
