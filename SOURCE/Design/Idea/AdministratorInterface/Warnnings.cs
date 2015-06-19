cee9d57b9ca34ea74fc9c7e8167722dfc51af3b1
Mikaeil <Michael.shaghelani1990@gmail.com>
2015-06-19 17:16:53 +0200

    Admin

---
+++ SOURCE/Design/Idea/AdministratorInterface/Warnnings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Classes;
+
+namespace Design.Idea.AdministratorInterface
+{
+    public partial class Warnnings : Form
+    {
+        public Warnnings()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Warnnings_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
