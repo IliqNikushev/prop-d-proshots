86c631d7932f1f812bd88bc4adcb67188573ef83
Iliq <iliq.nikushev@hotmail.com>
2015-06-20 16:53:08 +0200

    fix

---
+++ SOURCE/App-Admin/SubMenu/ShopInformation.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace App_Admin.SubMenu
+{
+    partial class ShopInformation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnBack = new System.Windows.Forms.PictureBox();
+            this.checkBoxVisitor = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.btnBack)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // findByNameTb
+            // 
+            this.findByNameTb.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.findByNameTb.Size = new System.Drawing.Size(130, 17);
+            // 
+            // findByTypeTb
+            // 
+            this.findByTypeTb.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.findByTypeTb.Size = new System.Drawing.Size(130, 17);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Items:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ALL",
+            "Cola",
+            "Red-bull",
+            "Ice-Tea",
+            "Orange juice"});
+            this.comboBox1.Location = new System.Drawing.Point(6, 46);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(70, 21);
+            this.comboBox1.TabIndex = 27;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 47);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(69, 20);
+            this.textBox1.TabIndex = 26;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(181, 46);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 20);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 78);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 303);
+            this.listBox1.TabIndex = 24;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Image = global::App_Admin.Properties.Resources.Back2;
+            this.btnBack.Location = new System.Drawing.Point(2, 2);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(37, 28);
+            this.btnBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnBack.TabIndex = 41;
+            this.btnBack.TabStop = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // checkBoxVisitor
+            // 
+            this.checkBoxVisitor.AutoSize = true;
+            this.checkBoxVisitor.Location = new System.Drawing.Point(85, 28);
+            this.checkBoxVisitor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBoxVisitor.Name = "checkBoxVisitor";
+            this.checkBoxVisitor.Size = new System.Drawing.Size(54, 17);
+            this.checkBoxVisitor.TabIndex = 42;
+            this.checkBoxVisitor.Text = "Visitor";
+            this.checkBoxVisitor.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.checkBoxVisitor);
+            this.groupBox1.Location = new System.Drawing.Point(2, 110);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(275, 384);
+            this.groupBox1.TabIndex = 43;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Overview Sorted by:";
+            // 
+            // ShopInformation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 542);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "ShopInformation";
+            this.Text = "ShopInformation";
+            this.Controls.SetChildIndex(this.findByNameLbl, 0);
+            this.Controls.SetChildIndex(this.findByTypeTb, 0);
+            this.Controls.SetChildIndex(this.findByTypeLbl, 0);
+            this.Controls.SetChildIndex(this.findByNameTb, 0);
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            this.Controls.SetChildIndex(this.btnBack, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.btnBack)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox btnBack;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBoxVisitor;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ SOURCE/App-Admin/SubMenu/Shops.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Classes;
+
+namespace App_Admin.SubMenu
+{
+    public partial class Shops : App_Common.HomePageWithMap
+    {
+        public Shops(App_Common.Menu parent) : base(parent)
+        {
+            Fix();
+            InitializeComponent();
+            Unfix();
+             
+             int xC = 0;
+             int yC = 0;
+             int countShops = 0;
+             Panel panel = new Panel();
+             panel.Left = 22;
+             panel.Top = 177;
+             panel.Width = 300;
+             panel.Height = 450;
+             panel.AutoScroll = true;
+             this.Controls.Add(panel);
+             
+
+                 foreach (var item in Database.Shops)
+                 {
+                     
+                     countShops++;
+                     PictureBox p = new PictureBox();
+                     p.ImageLocation = item.Icon;
+                     panel.Controls.Add(p);
+                     p.SizeMode = PictureBoxSizeMode.StretchImage;
+                     p.Width = 128;
+                     p.Height = 128;
+                     p.Left = xC;
+                     p.Top = yC;
+                     ShopWorkplace shop = item;
+                     p.Click += (x, y) => new ShopInformation(shop, this).Show();
+                     object rr = Database.ExecuteScalar("SELECT sum(TotalAmount) FROM `receiptitems` WHERE item_id in(select id from ShopItems where shopitems.shop_id= {0});", item.ID);
+                     if (rr==null)
+                     {
+                         rr = 0;
+                     }
+                     Label l = new Label();
+                     l.Left = xC;
+                     l.Top = yC + 128;
+                     l.Text = item.Label;
+                     panel.Controls.Add(l);
+                     Label prof = new Label();
+                     prof.Left = xC;
+                     prof.Top =yC+ 138 + l.Height;
+                     prof.Text = "Profit: " + rr.ToString()+Constants.Currency;
+                     panel.Controls.Add(prof);
+                     xC += 150;
+                     if (countShops % 2 == 0)
+                     {
+                         yC += 200;
+                         xC = 0;
+                     }
+
+                 }
+             PictureBox q = new PictureBox();
+             q.Image = Properties.Resources.KFC;
+             panel.Controls.Add(q);
+             q.SizeMode = PictureBoxSizeMode.StretchImage;
+             q.Width = 128;
+             q.Height = 128;
+             q.Left = xC;
+             q.Top = yC;
+             q.Parent = null;
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBack0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            //new ShopSubMenu(this).Show();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            new Calendar(this).Show();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click_1(object sender, EventArgs e)
+        {
+            new Camping(this).Show();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            new ServiceDesk(this).Show();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            new Visitors(this).Show();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            new PcDoctor(this).Show();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            new Employees(this).Show();
+        }
+
+        private void btnBack1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnBack_Click_2(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox4_Click_1(object sender, EventArgs e)
+        {
+            //new ShopInformation(Classes.Database.Find<Classes.ShopWorkplace>("|T|.id = 41"), this).Show();
+        }
+
+        private void pictureBox8_Click_1(object sender, EventArgs e)
+        {
+            //new ShopInformation(Classes.Database.Find<Classes.ShopWorkplace>("|T|.id = 41"), this).Show();
+        }
+
+        private void pictureBox5_Click_1(object sender, EventArgs e)
+        {
+            //new ShopInformation(Classes.Database.Find<Classes.ShopWorkplace>("|T|.id = 43"), this).Show();
+        }
+    }
+}
