4531f2db697b31b136bc669dd5579b5c5a8deb2b
Iliq Nikushev <iliq.nikushev@hotmail.com>
2017-12-05 18:26:03 +0100

    eror!

--- connect.php
+++ connect.php
@@ -2,9 +2,9 @@
 
 $connection = mysqli_connect('dbi321166.cq5dlpggp7y3.eu-central-1.rds.amazonaws.com:3306', 'dbi321166', 'parola12');
 if (!$connection) {
-    die("Database Connection Failed" . mysql_error());
+    die("Database Connection Failed" . mysql_error() . mysqli_connect_error());
 }
 $select_db = mysql_select_db('dbi321166');
 if (!$select_db) {
-    die("Database Selection Failed" . mysql_error());
+    die("Database Selection Failed" . mysql_error() . mysqli_connect_error());
 }
