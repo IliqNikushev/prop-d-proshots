e2493c411e6d68f3aecee51bdc10ea669ebb66c9
Iliq Nikushev <iliq.nikushev@hotmail.com>
2017-12-05 17:41:43 +0100

    *

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$connection = mysql_connect('aajn6jek5spgin.cq5dlpggp7y3.eu-central-1.rds.amazonaws.com', 'dbi321166', 'parola12');
+if (!$connection) {
+    die("Database Connection Failed" . mysql_error());
+}
+$select_db = mysql_select_db('dbi321166');
+if (!$select_db) {
+    die("Database Selection Failed" . mysql_error());
+}
