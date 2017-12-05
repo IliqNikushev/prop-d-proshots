3a2c35317da1da7516986bef0decebfdac5bdacc
Iliq Nikushev <iliq.nikushev@hotmail.com>
2017-12-05 18:00:39 +0100

    ^

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$connection = mysqli_connect('aajn6jek5spgin.cq5dlpggp7y3.eu-central-1.rds.amazonaws.com', 'dbi321166', 'parola12');
+$connection = mysqli_connect('dbi321166.cq5dlpggp7y3.eu-central-1.rds.amazonaws.com', 'dbi321166', 'parola12');
 if (!$connection) {
     die("Database Connection Failed" . mysql_error());
 }
